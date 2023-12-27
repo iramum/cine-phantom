82a52b02c62b277c909e0b8553f1266eb37f9fef
Anton Mashkov <slonikmak@gmail.com>
2023-12-28 02:02:53 +0300

    updated

---
+++ main.js
@@ -0,0 +1,152 @@
+import { loadPdf } from './pdfLoader.js';
+
+
+$(document).ready(function () {
+	console.log("Ready");
+
+	let $container = $('.magazine-container');
+
+	$('.year').click(function () {
+		var year = parseInt($(this).data('year'));
+		console.log('Year: ' + year);
+		$('.year').removeClass('selected-year');
+		// Add 'selected-year' class to the clicked div
+		$(this).addClass('selected-year');
+
+		$container.empty();
+
+		magazines.forEach(m => {
+			if (m.year === year) {
+				let block = generateMagazineBlock(m);
+				var $block = $container.append(block);
+
+				var $elem = $block.find(`#preview-${m.id}`);
+
+				loadPdf(m, img => {
+					$elem.empty();
+					$elem.append(`<img src="${img}"></img>`);
+				});
+
+				// $block.find(".left").click(e => {
+
+				// 	console.log("Click: " + m.id)
+
+				// 	var currentPage = !!m.currentPage ? m.currentPage : 0;
+				// 	if (currentPage == 0) {
+				// 		currentPage = m.pages - 1;
+				// 	} else {
+				// 		currentPage--;
+				// 	}
+				// 	var img = m.previews[currentPage];
+
+				// 	if (!img) {
+				// 		console.error("No preview found");
+				// 		console.log(m);
+				// 	} else {
+				// 		if ($elem.children().length === 0) {
+				// 			$elem.append(`<img src="${img}"></img>`);
+				// 		} else {
+				// 			$elem.find('img').attr('src', img);
+				// 		}
+				// 	}
+				// })
+			}
+		})
+
+	});
+
+
+	$container.on('click', '.left', function (e) {
+
+		var magazineId = $(this).closest('.m-block').attr('id');
+		console.log('click ' + magazineId);
+		var magazine = magazines.find(m => m.id === parseInt(magazineId));
+
+		if (!magazine) {
+			console.error('Magazine not found');
+			return;
+		}
+
+		var currentPage = magazine.currentPage ? magazine.currentPage : 0;
+		currentPage = (currentPage === 0) ? magazine.pages - 1 : currentPage - 1;
+
+		magazine.currentPage = currentPage;
+
+		var img = magazine.previews[currentPage];
+
+		if (!img) {
+			console.error("No preview found");
+			console.log(magazine);
+		} else {
+			var $elem = $(this).closest('.m-block').find("#preview-" + magazineId);
+			if ($elem.children().length === 0) {
+				$elem.append(`<img src="${img}"></img>`);
+			} else {
+				$elem.find('img').attr('src', img);
+			}
+		}
+	});
+
+	$container.on('click', '.right', function (e) {
+
+		var magazineId = $(this).closest('.m-block').attr('id');
+		console.log('click ' + magazineId);
+		var magazine = magazines.find(m => m.id === parseInt(magazineId));
+
+		if (!magazine) {
+			console.error('Magazine not found');
+			return;
+		}
+
+		var currentPage = magazine.currentPage ? magazine.currentPage : 0;
+		currentPage = (currentPage === magazine.pages - 1) ? 0 : currentPage + 1;
+
+		magazine.currentPage = currentPage;
+
+		var img = magazine.previews[currentPage];
+
+		if (!img) {
+			console.error("No preview found");
+			console.log(magazine);
+		} else {
+			var $elem = $(this).closest('.m-block').find("#preview-" + magazineId);
+			if ($elem.children().length === 0) {
+				$elem.append(`<img src="${img}"></img>`);
+			} else {
+				$elem.find('img').attr('src', img);
+			}
+		}
+	})
+
+});
+
+function generateMagazineBlock(magazine) {
+	let block = `
+            <div class="m-block" id="${magazine.id}">
+                <div class="div-6">
+                    <div class="div-wrapper-5">
+                        <div class="m-year">${magazine.year}</div>
+                    </div>
+                    <div class="div-7">
+                        <div class="text-wrapper-3 m-number">#${magazine.month}/${magazine.id}</div>
+                        <p class="text-wrapper-3 m-date">${magazine.displayData}</p>
+                    </div>
+                </div>
+                <div class="div-wrapper-6">
+                    <div class="text-wrapper-4 m-title">${magazine.title.toUpperCase()}</div>
+                </div>
+                <div class="cover-wrapper" id="preview-${magazine.id}"></canvas></div>
+                <div class="div-8">
+                    <div class="arrows-box">
+                        <img class="layer left" src="img/layer-1-1.svg" /> <img class="layer right" src="img/layer-1-1-1.svg" />
+                    </div>
+                    <div class="div-wrapper-7">
+                        <div class="text-wrapper-5 pdf-link"><a href="static/numbers/${magazine.id}.pdf">смотреть pdf-версию</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+
+	return block;
+}
---
+++ pdfLoader.js
@@ -0,0 +1,57 @@
+var { pdfjsLib } = globalThis;
+
+// The workerSrc property shall be specified.
+pdfjsLib.GlobalWorkerOptions.workerSrc = 'https://cdnjs.cloudflare.com/ajax/libs/pdf.js/4.0.269/pdf.worker.mjs';
+
+
+export async function loadPdf(model, callback) {
+
+	if (!model.previews) {
+
+		var url = `static/numbers/${model.id}.pdf`;
+
+		var pdf = await pdfjsLib.getDocument(url).promise;
+		console.log("loaded: " + model.id + ", pages: " + pdf.numPages);
+
+		var totalPages = pdf.numPages;
+
+		model.previews = [];
+		model.pages = totalPages;
+
+		for (let pageNum = 1; pageNum <= totalPages; pageNum++) {
+			var page = await pdf.getPage(pageNum);
+
+			var viewport = page.getViewport({ scale: 1 });
+
+			var height = viewport.height;
+			var scale = 500 / height;
+
+			viewport = page.getViewport({ scale: scale });
+
+			var canvas = document.createElement('canvas');
+			var context = canvas.getContext('2d');
+			canvas.height = viewport.height;
+			canvas.width = viewport.width;
+
+			await page.render({ canvasContext: context, viewport: viewport }).promise;
+
+			var base64Image = canvas.toDataURL('image/jpeg'); // Convert canvas to JPEG Base64
+			model.previews.push(base64Image);
+
+			console.log(model);
+
+			if (pageNum == 1) {
+				callback(base64Image);
+			}
+			// Clean up the canvas to free memory
+			context.clearRect(0, 0, canvas.width, canvas.height);
+			canvas = null;
+		}
+	} else {
+		callback(model.previews[0]);
+	}
+
+	console.log("Done!");
+
+}
+
